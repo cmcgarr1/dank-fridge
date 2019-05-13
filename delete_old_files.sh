3dc726dc7cbcb991ee1534052cbde844d87e3794
Connor <connor.mcgarr1.com>
2019-05-12 21:44:04 -0500

    fixed dropbox file saving/ moved delete folder

--- delete_old_files.sh
+++ delete_old_files.sh
@@ -1,4 +1,4 @@
 #!bin/sh
-find /home/pi/git/dank-fridge/temp_data_csv -type f -name "dank_fridge_$(date -d "$(date +%Y-%m)-15 last month" '+%Y_%-m')*" -exec rm {} +
-find /home/pi/git/dank-fridge/temp_data_csv -type f -name "batch_log*" -exec rm {} +
+find /home/pi/git/dank-fridge/files_to_delete -type f -name "dank_fridge_$(date -d "$(date +%Y-%m)-15 last month" '+%Y_%-m')*" -exec rm {} +
+find /home/pi/git/dank-fridge/files_to_delete -type f -name "batch_log*" -exec rm {} +
 
--- save_to_drop_box.py
+++ save_to_drop_box.py
@@ -3,36 +3,54 @@ import os
 import sys
 import datetime
 
-time_now=datetime.datetime.now()
-dropBox_directory="/dank-fridge/"
-home_directory="/home/pi/git/dank-fridge/temp_data_csv"
-
-file1 = open("/home/pi/git/dank-fridge/dropbox_key.txt","r")
-key_raw= file1.readlines()
-key_dbx=key_raw[0].strip('\n')
-
-dbx = dropbox.Dropbox(key_dbx)
-
-print('starting upload')
-print(str(time_now))
-
-for root, dirs, files in os.walk(home_directory):
-
-    for filename in files:
-        print(filename)
-        # construct the full local path
-        local_path = os.path.join(root, filename)
-
-        # construct the full Dropbox path
-        relative_path = os.path.relpath(local_path,home_directory)
-
-        dropbox_path = os.path.join(dropBox_directory, relative_path)
-
-        # upload the file
-        with open(local_path, 'rb') as f:
-            #dbx.put_file(dropbox_path, f)
-            dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite, autorename=True)
+def move_file(file_path,file_name):
+    delete_folder='/home/pi/git/dank-fridge/files_to_delete'
+    destination_path=os.path.join(delete_folder,file_name)
+    try:
+        os.rename(file_path,destination_path);
+        print("file "+file_name+" moved")
+    except:
+        pass
+def loop_drop_box(local_path, dropbox_path,dbx):
+    with open(local_path, 'rb') as f:
+        #dbx.put_file(dropbox_path, f)
+        dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode('overwrite'))
 
 
+def main():
+    time_now=datetime.datetime.now()
+    dropBox_directory="/dank-fridge/"
+    home_directory="/home/pi/git/dank-fridge/temp_data_csv"
 
-print('complete')
+    file1 = open("/home/pi/git/dank-fridge/dropbox_key.txt","r")
+    key_raw= file1.readlines()
+    key_dbx=key_raw[0].strip('\n')
+
+    dbx = dropbox.Dropbox(key_dbx)
+
+    print('starting upload')
+    print(str(time_now))
+
+    for root, dirs, files in os.walk(home_directory):
+        for filename in files:
+            # construct the full local path
+            local_path = os.path.join(root, filename)
+
+            # construct the full Dropbox path
+            relative_path = os.path.relpath(local_path,home_directory)
+
+            dropbox_path = os.path.join(dropBox_directory, relative_path)
+
+            # upload the file
+            if os.path.isfile(local_path):
+                print(filename)
+                loop_drop_box(local_path,dropbox_path,dbx)
+                move_file(local_path,filename)
+
+    print('complete')
+
+if __name__ == '__main__':
+    main()
+
+
+
